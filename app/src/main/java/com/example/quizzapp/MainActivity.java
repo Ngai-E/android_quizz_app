d449ac1b02ff4c39267350ddf951b0c285619494
Elizabeth <asobingai@gmail.com>
2019-11-25 05:20:41 +0100

    fix

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -5,7 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     1: a,b,c
+     2: a
+     3: b
+     4: b
+     5: c
+     6: a
+     7: c
+     8 : a
+     **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
