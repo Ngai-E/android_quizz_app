16d361f96183f86492c21c6a06dc01d5a9f8092d
Elizabeth <asobingai@gmail.com>
2019-12-09 00:06:37 +0100

    grade user

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.quizzapp;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.concurrent.TimeUnit;
+
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -16,9 +25,78 @@ public class MainActivity extends AppCompatActivity {
      8 : a
      **/
     int totalScore = 0;
+    String name = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submit (View view) {
+        //get the person's name
+       EditText editText = (EditText) findViewById(R.id.name);
+       name = editText.getText().toString();
+       gradeUser();
+       Toast toast = Toast.makeText(getApplicationContext(), "Sending results...", Toast.LENGTH_SHORT);
+       toast.show();
+    }
+
+    private void  gradeUser() {
+        CheckBox checkBox;
+        RadioButton radioButton;
+        int test = 0;
+
+        //grade question one
+        checkBox = (CheckBox) findViewById(R.id._1a);
+        if (checkBox.isChecked()) {
+            test++;
+        }
+        checkBox = (CheckBox) findViewById(R.id._1b);
+        if (checkBox.isChecked()) {
+            test++;
+        }
+        checkBox = (CheckBox) findViewById(R.id._1c);
+        if (checkBox.isChecked()) {
+            test++;
+        }
+        checkBox = (CheckBox) findViewById(R.id._1c);
+        if (checkBox.isChecked()) {
+            test = -1;
+        }
+
+        if (test == 3) {
+            this.totalScore++;
+        }
+
+        //grade question two
+        radioButton = (RadioButton) findViewById(R.id._2a);
+        if (radioButton.isChecked()) {
+            this.totalScore++;
+        }
+        //grade question three
+        radioButton = (RadioButton) findViewById(R.id._3b);
+        if (radioButton.isChecked()) {
+            this.totalScore++;
+        }
+        //grade question four
+        radioButton = (RadioButton) findViewById(R.id._4b);
+        if (radioButton.isChecked()) {
+            this.totalScore++;
+        }
+        //grade question five
+        radioButton = (RadioButton) findViewById(R.id._5c);
+        if (radioButton.isChecked()) {
+            this.totalScore++;
+        }
+        //grade question six
+        radioButton = (RadioButton) findViewById(R.id._6a);
+        if (radioButton.isChecked()) {
+            this.totalScore++;
+        }
+    }
+    public  void reset(View  view){
+     this.totalScore = 0;
+     this.name = "";
+    }
+
 }
